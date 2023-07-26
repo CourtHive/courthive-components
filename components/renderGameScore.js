5013232eb5fb90c9fc3b3b12a5f4b43740b94c36
courthive <charles@courthive.com>
2023-07-26 07:39:18 -0400

    style: :lipstick: score style tweaks

--- components/renderGameScore.js
+++ components/renderGameScore.js
@@ -1,30 +1,9 @@
-import { css } from "@stitches/core";
+import { gameScoreStyle } from "../styles/scoreStyles";
 
 export function renderGameScore({ value } = {}) {
-  const gameScoreStyle = css({
-    fontWeight: 500,
-    position: "relative",
-    margin: 0,
-    width: "$score$setWidth",
-    display: "flex",
-    justifyContent: "center",
-    fontSize: "$1",
-    lineHeight: "2.5rem",
-    variants: {
-      variant: {
-        winner: {
-          color: "$winner!important",
-          fontWeight: 700,
-        },
-        loser: {
-          color: "$loser!important",
-        },
-      },
-    },
-  });
-
   const input = document.createElement("input");
-  input.className = gameScoreStyle;
+  input.onclick = (e) => e.target.select();
+  input.className = gameScoreStyle();
   input.value = value;
 
   return input;
--- compositions/lineHeights.js
+++ compositions/lineHeights.js
@@ -1,4 +1,5 @@
 export const lineHeights = {
   scheduleInfo: 1.8,
   centerInfo: 1.5,
+  gameScore: 2,
 };
