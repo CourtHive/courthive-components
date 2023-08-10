75403628fc723593f00553412746ecea0527d156
courthive <charles@courthive.com>
2023-08-10 14:21:35 -0400

    feat: :sparkles: export `compositions`

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { renderParticipant } from './components/renderParticipant';
 export { renderStructure } from './components/renderStructure';
 export { renderMatchUp } from './components/renderMatchUp';
+export { compositions } from 'compositions/compositions';
