de74d1a1a5dbb4e6c4823e9f37cf016a7c50ec1b
courthive <charles@courthive.com>
2023-08-10 13:44:50 -0400

    ci: :construction_worker: install np; configure vite;

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { name } from './package.json';
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  server: {
+    host: '0.0.0.0'
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.js'),
+      fileName: (format) => `${name}.${format}.js`,
+      name
+    },
+    rollupOptions: {
+      external: [],
+      output: { globals: {} }
+    }
+  }
+});
