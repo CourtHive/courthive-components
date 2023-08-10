359b069a0a14d2e140555236e0cfa95a5717c520
courthive <charles@courthive.com>
2023-08-10 14:43:16 -0400

    chore: :package: correct exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { renderParticipant } from './components/renderParticipant';
 export { renderStructure } from './components/renderStructure';
 export { renderMatchUp } from './components/renderMatchUp';
-export { compositions } from 'compositions/compositions';
+export { compositions } from './compositions/compositions';
