d6e7cfbe862c3d727bc48f0b5a3d9d520a5cb52b
courthive <charles@courthive.com>
2023-08-11 09:11:57 -0400

    feat: :sparkles: additional exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 export { renderParticipant } from './components/renderParticipant';
 export { renderStructure } from './components/renderStructure';
+export { renderContainer } from 'components/renderContainer';
 export { renderMatchUp } from './components/renderMatchUp';
 export { compositions } from './compositions/compositions';
+export { cModal } from 'components/modal/cmodal';
