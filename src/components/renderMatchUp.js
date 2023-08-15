fa581a8313ab7b95de1d490fc178bd9fb4dfb569
courthive <charles@courthive.com>
2023-08-15 13:02:10 -0400

    feat: :construction: rough in score entry input

--- src/components/renderMatchUp.js
+++ src/components/renderMatchUp.js
@@ -1,43 +1,31 @@
-import { resultsInfoStyle, resultsItemStyle } from "../styles/resultStyles";
-import { getSelectedMatchUpStyle } from "../styles/getSelectedMatchUpStyle";
-import { matchUpContainerStyle } from "../styles/matchUpContainerStyle";
-import { getMatchUpStyle } from "../styles/getMatchUpStyle";
-import { renderCenterInfo } from "./renderCenterInfo";
-import { getLinkStyle } from "../styles/getLinkStyle";
-import { renderSide } from "./renderSide";
-import cx from "classnames";
+import { resultsInfoStyle, resultsItemStyle } from '../styles/resultStyles';
+import { getSelectedMatchUpStyle } from '../styles/getSelectedMatchUpStyle';
+import { matchUpContainerStyle } from '../styles/matchUpContainerStyle';
+import { getMatchUpStyle } from '../styles/getMatchUpStyle';
+import { renderCenterInfo } from './renderCenterInfo';
+import { getLinkStyle } from '../styles/getLinkStyle';
+import { renderSide } from './renderSide';
+import cx from 'classnames';
 
 export function renderMatchUp(params) {
-  const { composition, matchUp, moeity, selectedMatchUpId, searchActive } =
-    params;
-  const {
-    roundFactor,
-    roundNumber,
-    finishingRound,
-    matchUpType,
-    preFeedRound,
-    stage,
-  } = matchUp;
-  const qualifyingStage = stage === "QUALIFYING";
+  const { composition, matchUp, moeity, selectedMatchUpId, searchActive } = params;
+  const { roundFactor, roundNumber, finishingRound, matchUpType, preFeedRound, stage } = matchUp;
+  const qualifyingStage = stage === 'QUALIFYING';
   const isFinalRound = parseInt(finishingRound) === 1;
-  const isQualifying = stage === "QUALIFYING" && isFinalRound;
+  const isQualifying = stage === 'QUALIFYING' && isFinalRound;
 
   const noProgression = !qualifyingStage && isFinalRound;
   const isFirstRound = parseInt(roundNumber) === 1;
-  const isDoubles = matchUpType === "DOUBLES";
+  const isDoubles = matchUpType === 'DOUBLES';
   const link =
-    ((searchActive ||
-      matchUp.isRoundRobin ||
-      matchUp.collectionId ||
-      params.isLucky) &&
-      "mr") ||
-    (noProgression && "noProgression") ||
-    ((isQualifying || preFeedRound) && "m0") ||
-    (moeity && "m1") ||
-    "m2";
+    ((searchActive || matchUp.isRoundRobin || matchUp.collectionId || params.isLucky) && 'mr') ||
+    (noProgression && 'noProgression') ||
+    ((isQualifying || preFeedRound) && 'm0') ||
+    (moeity && 'm1') ||
+    'm2';
   const linkClass = getLinkStyle({ composition, isDoubles, roundFactor })({
     isFirstRound,
-    link,
+    link
   });
 
   const configuration = composition?.configuration || {};
@@ -45,27 +33,22 @@ export function renderMatchUp(params) {
 
   const eventHandlers = params.eventHandlers || {};
   const handleOnClick = (event) => {
-    if (typeof eventHandlers?.matchUpClick === "function") {
+    if (typeof eventHandlers?.matchUpClick === 'function') {
       eventHandlers.matchUpClick({ event, matchUp });
     }
   };
 
-  const container = document.createElement("div");
-  container.className = cx(
-    composition.theme,
-    params?.className,
-    "matchup",
-    matchUpContainerStyle()
-  );
+  const container = document.createElement('div');
+  container.className = cx(composition.theme, params?.className, 'matchup', matchUpContainerStyle());
   container.onclick = handleOnClick;
 
-  const component = document.createElement("div");
+  const component = document.createElement('div');
   component.className = getMatchUpStyle({ configuration });
 
   const entryStatusDisplay = ({ sideNumber }) => {
     const entryStatus = matchUp?.sides
       .find((s) => s.sideNumber === sideNumber)
-      ?.participant?.entryStatus?.replace("_", " ");
+      ?.participant?.entryStatus?.replace('_', ' ');
 
     const className = sideNumber === 2 && linkClass;
 
@@ -73,7 +56,7 @@ export function renderMatchUp(params) {
       eventHandlers,
       entryStatus,
       sideNumber,
-      className,
+      className
     });
   };
 
@@ -81,14 +64,14 @@ export function renderMatchUp(params) {
     sideNumber: 1,
     eventHandlers,
     composition,
-    matchUp,
+    matchUp
   });
   const side2 = renderSide({
     className: !centerInfo && linkClass,
     sideNumber: 2,
     eventHandlers,
     composition,
-    matchUp,
+    matchUp
   });
 
   component.appendChild(side1);
@@ -106,7 +89,7 @@ export function renderMatchUp(params) {
   container.appendChild(component);
 
   if (selectedMatchUpId === matchUp.matchUpId) {
-    const selected = document.createElement("div");
+    const selected = document.createElement('div');
     selected.className = getSelectedMatchUpStyle();
     container.appendChild(selected);
   }
@@ -115,26 +98,24 @@ export function renderMatchUp(params) {
 }
 
 function renderResultsInfo({ score }) {
-  const sets = score?.sets
-    ?.filter(Boolean)
-    .sort((a, b) => (a.setNumber || 0) - (b.setNumber || 0));
+  const sets = score?.sets?.filter(Boolean).sort((a, b) => (a.setNumber || 0) - (b.setNumber || 0));
   const finalSet = sets?.[sets.length - 1];
   const points = finalSet?.side1PointsScore || finalSet?.side2PointsScore;
 
-  const div = document.createElement("div");
+  const div = document.createElement('div');
   div.className = resultsInfoStyle();
 
   if (points) {
-    const pts = document.createElement("div");
-    pts.className = resultsItemStyle({ variant: "points" });
-    pts.innerHTML = "PTS";
+    const pts = document.createElement('div');
+    pts.className = resultsItemStyle({ variant: 'points' });
+    pts.innerHTML = 'PTS';
     div.appendChild(pts);
   }
 
   for (const set of sets || []) {
-    const setDiv = document.createElement("div");
-    setDiv.setAttribute("key", set.setNumber);
-    setDiv.className = resultsItemStyle({ variant: "set" });
+    const setDiv = document.createElement('div');
+    setDiv.setAttribute('key', set.setNumber);
+    setDiv.className = resultsItemStyle({ variant: 'set' });
     setDiv.innerHTML = set.setNumber;
     div.appendChild(setDiv);
   }
--- src/stories/matchUp.stories.js
+++ src/stories/matchUp.stories.js
@@ -1,46 +1,57 @@
-import { renderContainer } from "../components/renderContainer";
-import { renderMatchUp } from "../components/renderMatchUp";
-import { compositions } from "../compositions/compositions";
-import { generateMatchUps } from "../data/generateMatchUps";
+import { renderContainer } from '../components/renderContainer';
+import { renderMatchUp } from '../components/renderMatchUp';
+import { compositions } from '../compositions/compositions';
+import { generateMatchUps } from '../data/generateMatchUps';
 
 const argTypes = {
   composition: {
     options: Object.keys(compositions),
-    control: { type: "select" },
-  },
+    control: { type: 'select' }
+  }
 };
 
 export default {
-  title: "MatchUps/MatchUp",
-  tags: ["autodocs"],
+  title: 'MatchUps/MatchUp',
+  tags: ['autodocs'],
   render: ({ eventType, outcomes, randomWinningSide, ...args }) => {
-    const composition = compositions[args.composition || "Basic"];
+    const composition = compositions[args.composition || 'Basic'];
     const { matchUps } = generateMatchUps({
       ...args,
       randomWinningSide,
-      drawSize: 16,
+      drawSize: 2,
       eventType,
-      outcomes,
+      outcomes
     });
+    const matchUp = matchUps[0];
+    if (args.editing) {
+      matchUp.score.sets[0].editing = 2;
+    }
     const content = renderMatchUp({
       ...args,
-      matchUp: matchUps[0],
-      composition,
+      matchUp,
+      composition
     });
     return renderContainer({ theme: composition.theme, content });
   },
-  argTypes,
+  argTypes
 };
 
 export const Singles = {
   args: {
-    isLucky: true,
-  },
+    isLucky: true
+  }
 };
 
 export const Doubles = {
   args: {
-    eventType: "DOUBLES",
+    eventType: 'DOUBLES',
+    isLucky: true
+  }
+};
+
+export const Editing = {
+  args: {
     isLucky: true,
-  },
+    editing: 1
+  }
 };
