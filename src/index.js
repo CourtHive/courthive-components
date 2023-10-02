e6ee616e0a7773e869ab73a000f430c0c2b63f79
courthive <charles@courthive.com>
2023-10-02 10:55:21 -0400

    refactor: :recycle: correct import path for build

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 export { renderParticipant } from './components/renderParticipant';
-export { renderRoundHeader } from 'components/renderRoundHeader';
+export { renderRoundHeader } from './components/renderRoundHeader';
 export { renderStructure } from './components/renderStructure';
 export { renderContainer } from './components/renderContainer';
 export { renderMatchUp } from './components/renderMatchUp';
 export { compositions } from './compositions/compositions';
-export { renderRound } from 'components/renderRound';
+export { renderRound } from './components/renderRound';
 export { cModal } from './components/modal/cmodal';
