b9796ca63d06bdcd06f9ac8fff9f5fa6ba5547da
CourtHive <courthive@gmail.com>
2024-03-15 06:58:20 -0400

    chore: :package: upgrade to storybook 8

--- src/stories/tick.stories.js
+++ src/stories/tick.stories.js
@@ -1,13 +1,13 @@
-import { renderTick } from "../components/renderTick";
+import { renderTick } from '../components/renderTick';
 
 export default {
-  title: "MatchUps/Tick",
-  tags: ["autodocs"],
+  title: 'MatchUps/Tick',
+  tags: ['autodocs'],
   render: ({ ...args }) => {
     return renderTick({ ...args });
-  },
+  }
 };
 
 export const Tick = {
-  args: {},
+  args: {}
 };
