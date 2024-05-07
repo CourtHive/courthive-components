2e4d6d29f9d86daafcb9702da5cc64408c4e13f0
CourtHive <courthive@gmail.com>
2024-05-07 08:28:08 -0400

    refactor: :children_crossing: ability to specify minWidth for rounds

--- src/styles/roundHeaderStyle.js
+++ src/styles/roundHeaderStyle.js
@@ -5,6 +5,6 @@ export const roundHeaderStyle = css({
   marginInlineStart: '16px',
   marginInlineEnd: '16px',
   fontWeight: 'bold',
-  width: '370px',
+  // width: '370px',
   height: '1.5em'
 });
