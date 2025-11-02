d96b8e236cef225e20ec3a98fd7c3a51414e6f48
CourtHive <courthive@gmail.com>
2025-11-02 15:22:12 -0500

    refactor: :recycle: cleanup

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,21 @@
-import type { Preview } from '@storybook/html-vite'
+import type { Preview } from '@storybook/html-vite';
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
+        color: /(background|color)$/i,
+        date: /Date$/i
+      }
     },
-  },
+    backgrounds: {
+      default: 'light',
+      values: [
+        { name: 'light', value: '#ffffff' },
+        { name: 'dark', value: '#333333' }
+      ]
+    }
+  }
 };
 
 export default preview;
