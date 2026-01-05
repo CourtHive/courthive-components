2105ff777fba4e3b1acfaceebd5ee73b4af6f2ba
CourtHive <courthive@gmail.com>
2026-01-05 15:48:53 -0500

    feat: :sparkles: additional status pill support

--- src/stories/statusPill.stories.ts
+++ src/stories/statusPill.stories.ts
@@ -1,4 +1,7 @@
 import { renderStatusPill } from '../components/renderStatusPill';
+import { matchUpStatusConstants } from 'tods-competition-factory';
+
+const { WALKOVER, RETIRED, DEFAULTED, SUSPENDED, AWAITING_RESULT, CANCELLED, IN_PROGRESS } = matchUpStatusConstants;
 
 export default {
   title: 'MatchUps/Status',
@@ -10,12 +13,42 @@ export default {
 
 export const Walkover = {
   args: {
-    matchUpStatus: 'WALKOVER'
+    matchUpStatus: WALKOVER
+  }
+};
+
+export const Defaulted = {
+  args: {
+    matchUpStatus: DEFAULTED
+  }
+};
+
+export const Cancelled = {
+  args: {
+    matchUpStatus: CANCELLED
   }
 };
 
 export const Retirement = {
   args: {
-    matchUpStatus: 'RETIRED'
+    matchUpStatus: RETIRED
+  }
+};
+
+export const Awaiting = {
+  args: {
+    matchUpStatus: AWAITING_RESULT
+  }
+};
+
+export const InProgress = {
+  args: {
+    matchUpStatus: IN_PROGRESS
+  }
+};
+
+export const Suspended = {
+  args: {
+    matchUpStatus: SUSPENDED
   }
 };
--- src/styles/pillStyle.ts
+++ src/styles/pillStyle.ts
@@ -1,33 +1,51 @@
-import { css } from "@stitches/core";
+import { css } from '@stitches/core';
 
 export const pillStyle = css({
-  width: "auto",
-  display: "inline-block",
-  textTransform: "uppercase",
-  fontFamily: "Sharp Sans, Arial, sans-serif",
-  fontSize: "0.625rem",
-  lineHeight: "1rem",
-  marginInline: "0.25rem 0.25rem",
-  paddingInline: "0.25rem 0.25rem",
+  width: 'auto',
+  display: 'inline-block',
+  textTransform: 'uppercase',
+  fontFamily: 'Sharp Sans, Arial, sans-serif',
+  fontSize: '0.625rem',
+  lineHeight: '1rem',
+  marginInline: '0.25rem 0.25rem',
+  paddingInline: '0.25rem 0.25rem',
   fontWeight: 700,
-  borderRadius: "4px",
-  color: "#fff",
-  textAlign: "center",
-  whiteSpace: "nowrap",
+  borderRadius: '4px',
+  color: '#fff',
+  textAlign: 'center',
+  whiteSpace: 'nowrap',
   variants: {
     variant: {
+      in_progress: {
+        backgroundColor: '#199f01ff'
+      },
+      awaiting_result: {
+        backgroundColor: '#199f01ff'
+      },
       defaulted: {
-        backgroundColor: "#df164c",
+        backgroundColor: '#df164c'
+      },
+      dead_rubber: {
+        backgroundColor: '#df164c'
+      },
+      cancelled: {
+        backgroundColor: '#df164c'
+      },
+      incomplete: {
+        backgroundColor: '#f7ae04ff'
+      },
+      suspended: {
+        backgroundColor: '#f7ae04ff'
       },
       retired: {
-        backgroundColor: "#df164c",
+        backgroundColor: '#df164c'
       },
       walkover: {
-        backgroundColor: "black",
+        backgroundColor: 'black'
       },
       double_walkover: {
-        backgroundColor: "black",
-      },
-    },
-  },
+        backgroundColor: 'black'
+      }
+    }
+  }
 });
